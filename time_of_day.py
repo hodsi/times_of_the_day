c66c8a946915b6a24ecc6ffdc64f0218ff3d0bfc
Hod Sinay <sinayhod@gmail.com>
2020-01-08 20:08:59 +0200

    added a Time class for dealing better with time

--- time_of_day.py
+++ time_of_day.py
@@ -1,4 +1,12 @@
-from typing import List
+from __future__ import annotations
+
+from datetime import datetime, time, timedelta
+from typing import Union, List
+
+import consts
+
+MINUTES_IN_HOUR = 60
+HOURS_IN_DAY = 24
 
 
 class TimeOfDay(object):
@@ -8,3 +16,93 @@ class TimeOfDay(object):
 
     def __getitem__(self, item: str) -> str:
         return self.times_of_day[self.titles.index(item)]
+
+
+class Time(object):
+    def __init__(self, time_representer: Union[int, str, datetime, time, Time]):
+        self._total_minutes = None
+        if isinstance(time_representer, str):
+            time_representer = datetime.strptime(time_representer, consts.TIME_FORMAT)
+        if isinstance(time_representer, (datetime, time)):
+            self._total_minutes = time_representer.hour * MINUTES_IN_HOUR + time_representer.minute
+        if isinstance(time_representer, int):
+            self._total_minutes = time_representer % (HOURS_IN_DAY * MINUTES_IN_HOUR)
+        if isinstance(time_representer, Time):
+            self._total_minutes = time_representer._total_minutes
+        if self._total_minutes is None:
+            raise NotImplementedError
+
+    def __str__(self):
+        temp_datetime = datetime(1970, 1, 1)
+        temp_datetime += timedelta(minutes=self._total_minutes)
+        return temp_datetime.strftime(consts.TIME_FORMAT)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{str(self)}')"
+
+    def __iadd__(self, other: Union[int, str, datetime, time, Time]):
+        if not isinstance(other, Time) and isinstance(other, (int, str, datetime, time)):
+            self.__iadd__(Time(other))
+        elif isinstance(other, Time):
+            self._total_minutes += other._total_minutes
+            self._total_minutes %= HOURS_IN_DAY * MINUTES_IN_HOUR
+        else:
+            raise NotImplementedError
+        return self
+
+    def __add__(self, other: Union[int, str, datetime, time, Time]):
+        temp = Time(self)
+        temp += other
+        return temp
+
+    def __radd__(self, other):
+        return Time(other) + self
+
+    def __isub__(self, other: Union[int, str, datetime, time, Time]):
+        if not isinstance(other, Time) and isinstance(other, (int, str, datetime, time)):
+            self.__isub__(Time(other))
+        elif isinstance(other, Time):
+            self._total_minutes -= other._total_minutes
+            self._total_minutes %= HOURS_IN_DAY * MINUTES_IN_HOUR
+        else:
+            raise NotImplementedError
+        return self
+
+    def __sub__(self, other: Union[int, str, datetime, time, Time]):
+        temp_time = Time(self)
+        temp_time -= other
+        return temp_time
+
+    def __rsub__(self, other: Union[int, str, datetime, time, Time]):
+        return Time(other) - self
+
+    def __ifloordiv__(self, other: int):
+        if not isinstance(other, int):
+            raise NotImplementedError
+        self._total_minutes //= other
+        return self
+
+    def __floordiv__(self, other: int):
+        if not isinstance(other, int):
+            raise NotImplementedError
+        return Time(self._total_minutes // other)
+
+    def __imul__(self, other: int):
+        if not isinstance(other, int):
+            raise NotImplementedError
+        self._total_minutes *= other
+        return self
+
+    def __mul__(self, other: int):
+        if not isinstance(other, int):
+            raise NotImplementedError
+        return Time(self._total_minutes * other)
+
+    def __neg__(self):
+        return Time(0) - self
+
+    def __pos__(self):
+        return self
+
+    def __int__(self):
+        return self._total_minutes
